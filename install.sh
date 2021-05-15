47b67bd5df80b50544597a3dcf07c9c6b4959f66
Marcelus <marcelus20felipe@gmail.com>
2021-05-15 10:36:40 +0100

    started the install scripting

--- install.sh
+++ install.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
 #installer
+
+CreatePassEnvVariable="${CREATE_PASS:-~/.create-pass}"
+
+#Append this variable to the .bashrc
+#$CREATE_PASS_HOME=$CreatePassEnvVariable
+
+#export the variable to the .bashrc
+#~/.bashrc >> "PATH=$PATH:$CREATE_PASS_HOME/bin"
+
+#clear ./bashrc caches using source
+#source ~/.bashrc
