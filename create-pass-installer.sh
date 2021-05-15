9ec5afde1b19bd17a38e1db155f7c3d70bdc7a29
Marcelus <marcelus20felipe@gmail.com>
2021-05-15 16:12:39 +0100

    updated the create-pass script file

--- create-pass-installer.sh
+++ create-pass-installer.sh
@@ -4,31 +4,28 @@
 
 #create the folder ~/.create-pass
 ##echo "Creatig the folder ~/.create-pass"
-##mkdir ~/.create-pass
-
-echo "Checking for environment variable"
-##CreatePassEnvVariable="${CREATE_PASS_HOME:-$HOME/.create-pass}"
+##mkdir ~/.create-pass/bin
 
 #Append this variable to the .bashrc
 ##echo "Setting the CREATE_PASS_HOME variable"
-##echo "CREATE_PASS_HOME=$CreatePassEnvVariable" >> ~/.bashrc
+##echo "CREATE_PASS_HOME=~/.create-pass" >> ~/.bashrc
 
 
 #Download the python file from github to the folder ~/.create-pass folder
 ##echo "Downloading python script..."
-##wget https://raw.githubusercontent.com/marcelus20/create-pass/master/create-pass.py
+##wget https://raw.githubusercontent.com/marcelus20/create-pass/master/scripts/create-pass.py
 ##mv ./create-pass.py ~/.create-pass/
 
 
 #export the variable to the .bashrc
 ##echo "Appending the envirionment variable CREATE_PASS_HOME to the ~/.bashrc file"
-##echo "export PATH=$PATH:$CREATE_PASS_HOME/bin" >> ~/.bashrc
+##echo "export PATH=\$PATH:\$CREATE_PASS_HOME/bin" >> ~/.bashrc
 
 #clear ./bashrc caches using source
 ##source ~/.bashrc
 
 #Download the exec file to bin
 ##echo "Downloading script file..."
-##wget https://raw.githubusercontent.com/marcelus20/create-pass/master/create-pass
-##mv ./create-pass ~/.create-pass/
-##chmod +x ~/.create-pass/create-pass
+##wget https://raw.githubusercontent.com/marcelus20/create-pass/master/scripts/create-pass
+##mv ./create-pass ~/.create-pass/bin
+##chmod +x ~/.create-pass/bin/create-pass
